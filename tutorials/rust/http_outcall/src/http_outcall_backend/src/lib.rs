efd20567030ea1861dacb0577598517634e3a182
linnefromice <linnefromice@gmail.com>
2023-04-05 16:40:11 +0900

    update: impl counter

--- tutorials/rust/http_outcall/src/http_outcall_backend/src/lib.rs
+++ tutorials/rust/http_outcall/src/http_outcall_backend/src/lib.rs
@@ -1,4 +1,46 @@
-#[ic_cdk_macros::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use std::cell::RefCell;
+use candid::types::number::Nat;
+use ic_cdk_macros::{query, update};
+
+thread_local! {
+    static COUNTER: RefCell<Nat> = RefCell::new(Nat::from(0))
+}
+
+#[query]
+fn get() -> Nat {
+    COUNTER.with(|counter| (*counter.borrow()).clone())
+}
+
+#[update]
+fn set(n: Nat) {
+    COUNTER.with(|count| *count.borrow_mut() = n);
+}
+
+#[update]
+fn inc() {
+    COUNTER.with(|count| *count.borrow_mut() += 1);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_set() {
+        let expected = Nat::from(42);
+        set(expected.clone());
+        assert_eq!(get(), expected);
+    }
+    #[test]
+    fn test_init() {
+        assert_eq!(get(), Nat::from(0));
+    }
+
+    #[test]
+    fn test_inc() {
+        for i in 1..10 {
+            inc();
+            assert_eq!(get(), Nat::from(i));
+        }
+    }
 }
