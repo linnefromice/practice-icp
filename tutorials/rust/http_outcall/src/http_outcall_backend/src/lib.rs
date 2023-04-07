69bc325b88f5f255f1a9d3815d463f9e57bbd493
linnefromice <linnefromice@gmail.com>
2023-04-07 10:29:32 +0900

    update: http_outcall - call eth_blockNumber

--- tutorials/rust/http_outcall/src/http_outcall_backend/src/lib.rs
+++ tutorials/rust/http_outcall/src/http_outcall_backend/src/lib.rs
@@ -1,7 +1,8 @@
-use std::{cell::RefCell, future::Future};
+use std::cell::RefCell;
 use candid::types::number::Nat;
-use ic_cdk::api::{management_canister::http_request::{HttpHeader, CanisterHttpRequestArgument, HttpMethod, HttpResponse, http_request}, call::CallResult};
+use ic_cdk::api::management_canister::http_request::{HttpHeader, CanisterHttpRequestArgument, HttpMethod, http_request};
 use ic_cdk_macros::{query, update};
+use serde_json::{json, Value};
 
 thread_local! {
     static COUNTER: RefCell<Nat> = RefCell::new(Nat::from(0))
@@ -24,15 +25,63 @@ fn inc() {
 
 #[query]
 async fn call_api_query() -> String {
-    call_binance_api().await
+    call_binance_api_internal().await
 }
 
 #[update]
-async fn call_api() -> String {
-    call_binance_api().await
+async fn call_binance_api() -> String {
+    call_binance_api_internal().await
 }
 
-async fn call_binance_api() -> String {
+#[update]
+async fn call_eth_block_number() -> String {
+    call_eth_block_number_internal().await
+}
+
+async fn call_eth_block_number_internal() -> String {
+    let host = "polygon-mainnet.g.alchemy.com";
+    let mut host_header = host.clone().to_owned();
+    host_header.push_str(":443");
+    let request_headers = vec![
+        HttpHeader {
+            name: "Host".to_string(),
+            value: host_header,
+        },
+        HttpHeader {
+            name: "User-Agent".to_string(),
+            value: "http_outcall_backend_canister".to_string()
+        }
+    ];
+
+    let json_payload = json!({
+        "jsonrpc": "2.0",
+        "method": "eth_blockNumber",
+        "id": 1
+    });
+    let body = serde_json::to_vec(&json_payload).unwrap();
+    let url = format!("https://{host}/v2/sLp6VfuskMEwx8Wx0DvaRkI8qCoVYF8f");
+        let request = CanisterHttpRequestArgument {
+        url,
+        method: HttpMethod::POST,
+        body: Some(body),
+        max_response_bytes: None,
+        transform: None,
+        headers: request_headers,
+    };
+    match http_request(request).await {
+        Ok((response,)) => {
+            let json: Value = serde_json::from_slice(&response.body).expect("Transformed response is not JSON payload.");
+            let result = json.get("result").unwrap().as_str().unwrap();
+            let block_number = u64::from_str_radix(&result[2..], 16).unwrap();
+            block_number.to_string()
+        },
+        Err((_, m)) => {
+            m
+        }
+    }
+}
+
+async fn call_binance_api_internal() -> String {
     let host = "www.binance.us";
     // let host = "randomuser.me";
     let mut host_header = host.clone().to_owned();
