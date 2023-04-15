e5fc00417c42ffeb785f7bdff2339e88aad783d8
linnefromice <linnefromice@gmail.com>
2023-04-15 17:20:27 +0900

    update: rate state canister

--- various/rust/poc-sync-to-chain-from-canister/icp_synchronizer/src/rate_v1/src/lib.rs
+++ various/rust/poc-sync-to-chain-from-canister/icp_synchronizer/src/rate_v1/src/lib.rs
@@ -1,8 +1,9 @@
-use std::{cell::RefCell, ops::Deref};
+use std::cell::RefCell;
 use candid::CandidType;
 use ic_cdk::{api, update, query};
+use ic_cdk_timers::TimerId;
 
-#[derive(Clone, Default, CandidType)]
+#[derive(Clone, Debug, Default, CandidType)]
 pub struct Round {
     pub round_id: u128,
     pub answer: i128,
@@ -14,6 +15,7 @@ thread_local! {
     static LATEST_ROUND_ID: RefCell<u128> = RefCell::default();
     static ROUNDS: RefCell<Vec<Round>> = RefCell::default();
     static SYNCED_LATEST_ROUND_ID: RefCell<u128> = RefCell::default();
+    static TIMER_ID: RefCell<TimerId> = RefCell::default(); // for debug
 }
 
 #[update]
@@ -21,7 +23,6 @@ fn update_state(answer: i128) -> Round {
     let timestamp = api::time();
     update_state_internal(answer, timestamp, timestamp)
 }
-
 fn update_state_internal(answer: i128, started_at: u64, updated_at: u64) -> Round {
     let incremented_round_id = LATEST_ROUND_ID.with(|val| {
         let mut mut_ref = val.borrow_mut();
@@ -38,6 +39,50 @@ fn update_state_internal(answer: i128, started_at: u64, updated_at: u64) -> Roun
     round
 }
 
+#[update]
+fn periodic_sync_state() {
+    periodic_sync_state_internal(30, 5)
+}
+
+fn periodic_sync_state_internal(interval_secs: u64, max_run_unit: u128) {
+    let interval = std::time::Duration::from_secs(interval_secs);
+    let max_run_unit_owned = std::sync::Arc::new(max_run_unit);
+
+    let timer_id = ic_cdk_timers::set_timer_interval(interval, move || {
+        ic_cdk::println!("[START] Synchronization");
+        let synced_latest_round_id = get_synced_latest_round_id();
+        let latest_round_id = get_latest_round_id();
+        if synced_latest_round_id == latest_round_id {
+            ic_cdk::println!("Already synced: round_id is {:?}", synced_latest_round_id);
+            return
+        }
+        let not_synced = latest_round_id - synced_latest_round_id;
+        let run_unit = if not_synced > *max_run_unit_owned { *max_run_unit_owned } else { not_synced };
+
+        for i in 0..run_unit {
+            let round = get_round(synced_latest_round_id+1+i-1);
+            ic_cdk::println!("sync round: {:?}", round);
+        }
+
+        SYNCED_LATEST_ROUND_ID.with(|value| *value.borrow_mut() += run_unit);
+        ic_cdk::println!("[FINISH] Synchronization");
+    });
+
+    TIMER_ID.with(|value| *value.borrow_mut() = timer_id);
+}
+
+fn get_latest_round_id() -> u128 {
+    LATEST_ROUND_ID.with(|value| (*value.borrow()).clone())
+}
+fn get_round(idx: u128) -> Round {
+    ROUNDS.with(|rounds| {
+        let rounds = rounds.borrow();
+        rounds[idx as usize].clone()
+    })
+}
+fn get_synced_latest_round_id() -> u128 {
+    SYNCED_LATEST_ROUND_ID.with(|value| (*value.borrow()).clone())
+}
 
 #[update]
 fn debug_update_state(answer: i128, started_at: u64, updated_at: u64) -> Round {
@@ -46,14 +91,11 @@ fn debug_update_state(answer: i128, started_at: u64, updated_at: u64) -> Round {
 
 #[query]
 fn debug_latest_round_id() -> u128 {
-    LATEST_ROUND_ID.with(|value| (*value.borrow()).clone())
+    get_latest_round_id()
 }
 #[query]
 fn debug_round(idx: u128) -> Round {
-    ROUNDS.with(|rounds| {
-        let rounds = rounds.borrow();
-        rounds[idx as usize].clone()
-    })
+    get_round(idx)
 }
 #[query]
 fn debug_rounds_length() -> u128 {
@@ -61,5 +103,14 @@ fn debug_rounds_length() -> u128 {
 }
 #[query]
 fn debug_synced_latest_round_id() -> u128 {
-    SYNCED_LATEST_ROUND_ID.with(|value| (*value.borrow()).clone())
+    get_synced_latest_round_id()
+}
+#[update]
+fn debug_periodic_sync_state(interval_secs: u64, max_run_unit: u128) {
+    periodic_sync_state_internal(interval_secs, max_run_unit)
+}
+#[update]
+fn debug_stop_timer() {
+    let timer_id = TIMER_ID.with(|value| value.borrow().clone());
+    ic_cdk_timers::clear_timer(timer_id);
 }
