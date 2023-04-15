8e2b9fef26a4afc2da3380e6927facd66d9fc69e
linnefromice <linnefromice@gmail.com>
2023-04-15 15:41:06 +0900

    update: canister - enable to set gas_limit

--- various/rust/poc-sync-to-chain-from-canister/icp_synchronizer/src/synchronizer_v1/src/lib.rs
+++ various/rust/poc-sync-to-chain-from-canister/icp_synchronizer/src/synchronizer_v1/src/lib.rs
@@ -1,6 +1,6 @@
 mod utils;
 
-use std::{str::FromStr, sync::atomic::{AtomicU64, Ordering}};
+use std::{str::FromStr, sync::atomic::{AtomicU64, Ordering}, ops::{Mul, Div}};
 use candid::CandidType;
 use ic_cdk::{query, update, api::management_canister::http_request::{TransformArgs, HttpResponse}};
 use ic_web3::{Web3, types::{Address, SignedTransaction, U64, U256, TransactionParameters}, transports::ICHttp, contract::{Contract, Options, tokens::Tokenize}, ic::{get_eth_addr, KeyInfo}};
@@ -48,6 +48,9 @@ async fn update_state(
     answer: i128, // TODO: i256
     started_at: u128, // TODO: u256
     updated_at: u128, // TODO: u256
+    gas_coefficient_molecule: u128,
+    gas_coefficient_denominator: u128,
+    gas_limit: Option<u128>
 ) -> Result<String, String> {
     let w3 = generate_web3_client()
         .map_err(|e| format!("generate_web3_client failed: {}", e))?;
@@ -56,7 +59,10 @@ async fn update_state(
         id,
         answer,
         started_at,
-        updated_at
+        updated_at,
+        gas_coefficient_molecule,
+        gas_coefficient_denominator,
+        gas_limit
     ).await?;
     match w3.eth().send_raw_transaction(signed_tx.raw_transaction).await {
         Ok(v) => Ok(format!("0x{}", hex::encode(v))),
@@ -70,13 +76,18 @@ async fn update_state_signed_tx_internal(
     answer: i128, // TODO: i256
     started_at: u128, // TODO: u256
     updated_at: u128, // TODO: u256
+    gas_coefficient_molecule: u128,
+    gas_coefficient_denominator: u128,
+    gas_limit: Option<u128>,
 ) -> Result<SignedTransaction, String> {
     sign(
         w3,
         &ORACLE_ADDR,
         &ORACLE_ABI,
         &"updateState",
-        (id,answer,started_at,updated_at,)
+        (id,answer,started_at,updated_at,),
+        (gas_coefficient_molecule, gas_coefficient_denominator),
+        gas_limit
     ).await
 }
 
@@ -86,6 +97,8 @@ async fn sign(
     abi: &[u8],
     func: &str,
     params: impl Tokenize,
+    gas_coefficient: (u128, u128),
+    gas_limit: Option<u128>,
 ) -> Result<SignedTransaction, String> {
     let contract = generate_contract_client(w3.clone(), contract_addr, abi)
         .map_err(|e| format!("generate_contract_client failed: {}", e))?;
@@ -102,9 +115,19 @@ async fn sign(
         .map_err(|e| format!("get gas_price error: {}", e))?;
     let options = Options::with(|op| {
         op.nonce = Some(tx_count);
-        op.gas_price = Some(gas_price);
-        op.transaction_type = Some(U64::from(2)) // EIP1559_TX_ID
+        op.gas_price = Some(gas_price.mul(U256::from(gas_coefficient.0)).div(U256::from(gas_coefficient.1)));
+        op.transaction_type = Some(U64::from(2)); // EIP1559_TX_ID
+
+        // temp
+        if let Some(gas_limit_value) = gas_limit {
+            op.gas = Some(U256::from(gas_limit_value))
+        }
     });
+    // TODO: consider gas_limit
+    // let estimated_gas = contract.estimate_gas(func, params.clone(), canister_addr, options)
+    //     .await
+    //     .map_err(|e| format!("estimate_gas error: {}", e))?;
+    // options.gas = Some(estimated_gas);
 
     match contract.sign(
         func,
@@ -154,6 +177,9 @@ async fn debug_update_state_signed_tx(
     answer: i128, // TODO: i256
     started_at: u128, // TODO: u256
     updated_at: u128, // TODO: u256
+    gas_coefficient_molecule: u128,
+    gas_coefficient_denominator: u128,
+    gas_limit: Option<u128>,
 ) -> Result<CandidSignedTransaction, String> {
     let w3 = generate_web3_client()
         .map_err(|e| format!("generate_web3_client failed: {}", e))?;
@@ -163,6 +189,9 @@ async fn debug_update_state_signed_tx(
         answer,
         started_at,
         updated_at,
+        gas_coefficient_molecule,
+        gas_coefficient_denominator,
+        gas_limit
     ).await {
         Ok(signed_tx) =>
             Ok(CandidSignedTransaction {
@@ -176,6 +205,44 @@ async fn debug_update_state_signed_tx(
         Err(msg) => Err(msg)
     }
 }
+#[update]
+async fn debug_update_state_estimate_gas(
+    id: u128, // TODO: u256
+    answer: i128, // TODO: i256
+    started_at: u128, // TODO: u256
+    updated_at: u128, // TODO: u256
+    gas_coefficient_molecule: u128,
+    gas_coefficient_denominator: u128,
+) -> Result<String, String> {
+    let gas_coefficient = (gas_coefficient_molecule, gas_coefficient_denominator);
+
+    let w3 = generate_web3_client()
+        .map_err(|e| format!("generate_web3_client failed: {}", e))?;
+    let contract = generate_contract_client(w3.clone(), &ORACLE_ADDR, &ORACLE_ABI)
+    .map_err(|e| format!("generate_contract_client failed: {}", e))?;
+    let canister_addr = get_eth_addr(None, None, KEY_NAME.to_string()).await
+        .map_err(|e| format!("get_eth_addr failed: {}", e))?;
+
+    let tx_count = w3.eth()
+        .transaction_count(canister_addr, None)
+        .await
+        .map_err(|e| format!("get tx count error: {}", e))?;
+    let gas_price = w3.eth()
+        .gas_price()
+        .await
+        .map_err(|e| format!("get gas_price error: {}", e))?;
+    let options = Options::with(|op| {
+        op.nonce = Some(tx_count);
+        op.gas_price = Some(gas_price.mul(U256::from(gas_coefficient.0)).div(U256::from(gas_coefficient.1)));
+        op.transaction_type = Some(U64::from(2)) // EIP1559_TX_ID
+    });
+
+    let estimated_gas = contract.estimate_gas(&"updateState", (id,answer,started_at,updated_at,), canister_addr, options)
+        .await
+        .map_err(|e| format!("estimate_gas error: {}", e))?;
+    Ok(estimated_gas.to_string())
+}
+
 #[update]
 async fn debug_balance_of_native() -> Result<String, String> {
     let w3 = generate_web3_client()
