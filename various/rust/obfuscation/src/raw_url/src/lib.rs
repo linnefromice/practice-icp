7191d60dc128228fae8e994cf93533dab9e726eb
linnefromice <linnefromice@gmail.com>
2023-04-20 12:07:03 +0900

    update

--- various/rust/obfuscation/src/raw_url/src/lib.rs
+++ various/rust/obfuscation/src/raw_url/src/lib.rs
@@ -6,3 +6,13 @@ const URL: &'static str = "https://polygon-mumbai.g.alchemy.com/v2/6GLIzI5pL0n4b
 fn rpc_endpoint() -> String {
     URL.to_string()
 }
+
+#[query]
+fn to_hex_str() -> String {
+    hex::encode(URL.as_bytes())
+}
+
+#[query]
+fn to_bytes() -> Vec<u8> {
+    hex::decode(to_hex_str()).unwrap()
+}
