b81adcfd1504a9a2e1cecb8ee318a5f7f71b69d5
linnefromice <linnefromice@gmail.com>
2023-04-25 23:37:42 +0900

    update: deploy script

--- with-uniswap/synchronizers/deploy.sh
+++ with-uniswap/synchronizers/deploy.sh
@@ -4,3 +4,7 @@ dfx deploy reader_mainnet \
 dfx deploy reader_polygon \
     --argument '("https://polygon-mainnet.g.alchemy.com/v2/sLp6VfuskMEwx8Wx0DvaRkI8qCoVYF8f","45dda9cb7c25131df268515131f647d726f50608")'
 dfx deploy calculator
+dfx canister call reader_mainnet periodic_save_prices "(null,null,null)"
+dfx canister call reader_polygon periodic_save_prices "(null,null,null)"
+dfx canister call calculator register_reader "(principal \"$(dfx canister id reader_mainnet)\")"
+dfx canister call calculator register_reader "(principal \"$(dfx canister id reader_polygon)\")"
