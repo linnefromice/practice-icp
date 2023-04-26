080da949d4dc46f1894fec869766bbd56c58e8ba
linnefromice <linnefromice@gmail.com>
2023-04-26 15:38:45 +0900

    update: calculator

--- with-uniswap/synchronizers/deploy.sh
+++ with-uniswap/synchronizers/deploy.sh
@@ -3,7 +3,8 @@ dfx deploy reader_mainnet \
     --argument '("https://eth-mainnet.g.alchemy.com/v2/JVUDgQSB0r-3HhohPCod6uBy_Zx8WEdy","88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640")'
 dfx deploy reader_polygon \
     --argument '("https://polygon-mainnet.g.alchemy.com/v2/sLp6VfuskMEwx8Wx0DvaRkI8qCoVYF8f","45dda9cb7c25131df268515131f647d726f50608")'
-dfx deploy calculator
+dfx deploy calculator \
+    --argument '(6, 18)'
 dfx canister call reader_mainnet periodic_save_prices "(null,null,null)"
 dfx canister call reader_polygon periodic_save_prices "(null,null,null)"
 dfx canister call calculator register_reader "(principal \"$(dfx canister id reader_mainnet)\")"
--- with-uniswap/synchronizers/src/uniswap_calculator_v1/src/debug.rs
+++ with-uniswap/synchronizers/src/uniswap_calculator_v1/src/debug.rs
@@ -1,6 +1,11 @@
-use ic_cdk::{api::call, update};
+use candid::Principal;
+use ic_cdk::{api::call, query, update};
+use ic_web3::types::U256;
 
-use crate::READER_CANISTERS;
+use crate::{
+    call_get_prices_in_reader, get_prices_from_all_registered, token0_decimals, token1_decimals,
+    utils::calculate_exchange_rate, CandidPrice, READER_CANISTERS,
+};
 
 #[update]
 async fn debug_rpcs() -> Vec<(String, String)> {
@@ -11,4 +16,34 @@ async fn debug_rpcs() -> Vec<(String, String)> {
         results.push((canister.to_string(), result.unwrap().0));
     }
     results
+}
+#[query]
+fn debug_tokens_decimals() -> (u8, u8) {
+    (token0_decimals(), token1_decimals())
+}
+
+#[update]
+async fn debug_get_prices_from_all_registered(
+    from: Option<u32>,
+    to: Option<u32>,
+) -> Vec<Vec<CandidPrice>> {
+    get_prices_from_all_registered(from, to).await
+}
+#[update]
+async fn debug_call_get_prices_in_reader(
+    canister_id: Principal,
+    from: Option<u32>,
+    to: Option<u32>,
+) -> Vec<CandidPrice> {
+    call_get_prices_in_reader(canister_id, from, to).await
+}
+#[query]
+fn debug_calculate_exchange_rate(sqrt_price_x96: String, precision: u8) -> String {
+    let result = calculate_exchange_rate(
+        U256::from_dec_str(&sqrt_price_x96).unwrap(),
+        token0_decimals(),
+        token1_decimals(),
+        precision,
+    );
+    result.to_string()
 }
