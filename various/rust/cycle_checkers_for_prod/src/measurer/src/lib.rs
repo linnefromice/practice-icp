2131228cce24306b6b99e74cef39e07fdad0e2b0
linnefromice <linnefromice@gmail.com>
2023-04-28 13:54:06 +0900

    update

--- various/rust/cycle_checkers_for_prod/src/measurer/src/lib.rs
+++ various/rust/cycle_checkers_for_prod/src/measurer/src/lib.rs
@@ -5,8 +5,10 @@ use candid::CandidType;
 use sha2::Digest;
 use ic_cdk::{api::management_canister::{http_request::{TransformArgs, HttpResponse}, ecdsa::{SignWithEcdsaArgument, sign_with_ecdsa, SignWithEcdsaResponse, EcdsaCurve, EcdsaKeyId}}};
 use ic_cdk_macros::{query, update};
-use ic_web3::{ic::{get_eth_addr, KeyInfo}, types::{Address, TransactionParameters, U256, SignedTransaction}, Web3, transports::ICHttp};
-use utils::{generate_web3_client, KN_IN_LOCAL, default_derivation_key, get_public_key, CHAIN_ID, pubkey_to_address};
+use ic_web3::{ic::{get_eth_addr, KeyInfo}, types::{Address, TransactionParameters, U256, SignedTransaction, U64}, Web3, transports::ICHttp, contract::{tokens::Tokenize, Options}};
+use utils::{generate_web3_client, KN_IN_LOCAL, default_derivation_key, get_public_key, CHAIN_ID, pubkey_to_address, generate_contract_client};
+
+pub const ERC20_ABI: &[u8] = include_bytes!("../../abi/erc20.json");
 
 #[query]
 fn transform(response: TransformArgs) -> HttpResponse {
@@ -100,7 +102,21 @@ async fn sign_message(
 }
 
 #[update]
-async fn sign_transfer_native(to: String, value: u64, tx_count: u128, gas_price: u128, max_resp: u64) -> Result<String, String> {
+async fn balance_of_native() -> Result<String, String> {
+    let w3 = generate_web3_client(Some(300), None)
+        .map_err(|e| format!("generate_web3_client failed: {}", e))?;
+    let canister_addr = get_eth_addr(None, None, KN_IN_LOCAL.to_string()).await
+        .map_err(|e| format!("get_eth_addr failed: {}", e))?;
+    let balance = w3
+        .eth()
+        .balance(canister_addr, None)
+        .await
+        .map_err(|e| format!("get balance failed: {}", e))?;
+    Ok(balance.to_string())
+}
+
+#[update]
+async fn sign_transfer_native(to: String, value: u64, tx_count: Option<u128>, gas_price: Option<u128>, max_resp: u64) -> Result<String, String> {
     let w3 = generate_web3_client(
         Some(max_resp),
         None,
@@ -111,7 +127,7 @@ async fn sign_transfer_native(to: String, value: u64, tx_count: u128, gas_price:
 }
 
 #[update]
-async fn transfer_native(to: String, value: u64, tx_count: u128, gas_price: u128, max_resp: u64) -> Result<String, String> {
+async fn transfer_native(to: String, value: u64, tx_count: Option<u128>, gas_price: Option<u128>, max_resp: u64) -> Result<String, String> {
     let w3 = generate_web3_client(
         Some(max_resp),
         None,
@@ -119,23 +135,35 @@ async fn transfer_native(to: String, value: u64, tx_count: u128, gas_price: u128
         .map_err(|e| format!("generate_web3_client failed: {}", e))?;
     let signed_tx = sign_transfer_native_internal(w3.clone(), to, value, tx_count, gas_price).await.unwrap();
     match w3.eth().send_raw_transaction(signed_tx.raw_transaction).await {
-        Ok(txhash) => {
-            ic_cdk::println!("txhash: {}", hex::encode(txhash.0));
-            Ok(format!("{}", hex::encode(txhash.0)))
-        },
+        Ok(txhash) => { Ok(format!("0x{}", hex::encode(txhash.0))) },
         Err(e) => { Err(e.to_string()) },
     }
 }
 
-async fn sign_transfer_native_internal(w3: Web3<ICHttp>, to: String, value: u64, tx_count: u128, gas_price: u128) -> Result<SignedTransaction, String> {
+async fn sign_transfer_native_internal(w3: Web3<ICHttp>, to: String, value: u64, tx_count: Option<u128>, gas_price: Option<u128>) -> Result<SignedTransaction, String> {
     let canister_addr = get_eth_addr(None, None, KN_IN_LOCAL.to_string()).await
         .map_err(|e| format!("get_eth_addr failed: {}", e))?;
 
+    let tx_count = match tx_count {
+        Some(v) => U256::from(v),
+        None => w3.eth()
+            .transaction_count(canister_addr, None)
+            .await
+            .map_err(|e| format!("get tx count error: {}", e))?
+    };
+    let gas_price = match gas_price {
+        Some(v) => U256::from(v),
+        None => w3.eth()
+            .gas_price()
+            .await
+            .map_err(|e| format!("get gas_price error: {}", e))?
+    };
+
     let tx = TransactionParameters {
         to: Some(Address::from_str(&to).unwrap()),
-        nonce: Some(U256::from(tx_count)),
+        nonce: Some(tx_count),
         value: U256::from(value),
-        gas_price: Some(U256::from(gas_price)),
+        gas_price: Some(gas_price),
         gas: U256::from(21000),
         ..Default::default()
     };
@@ -149,4 +177,125 @@ async fn sign_transfer_native_internal(w3: Web3<ICHttp>, to: String, value: u64,
         .await
         .map_err(|e| format!("sign_transaction error: {}", e))?;
     Ok(signed_tx)
+}
+
+#[update]
+async fn balance_of_erc20(token_addr: String, account_addr: Option<String>, max_resp: Option<u64>) -> Result<String, String> {
+    let max_resp = if max_resp.is_some() {
+        max_resp
+    } else {
+        Some(400) // default
+    };
+    let w3 = generate_web3_client(max_resp, None)?;
+    let contract = generate_contract_client(w3.clone(), &token_addr, ERC20_ABI)
+        .map_err(|e| format!("generate_contract_client failed: {}", e))?;
+    let account_addr = match account_addr {
+        Some(v) => Address::from_str(&v).unwrap(),
+        None => get_eth_addr(None, None, KN_IN_LOCAL.to_string()).await
+            .map_err(|e| format!("get_eth_addr failed: {}", e))?,
+    };
+    let res: U256 = contract
+        .query(
+            "balanceOf",
+            account_addr,
+            None,
+            Options::default(),
+            None,
+        )
+        .await
+        .map_err(|e| format!("query contract error: {}", e))?;
+    Ok(res.to_string())
+}
+
+#[update]
+async fn sign_transfer_erc20(token_addr: String, to_addr: String, value: u64, tx_count: Option<u128>, gas_price: Option<u128>, max_resp: u64) -> Result<String, String> {
+    let w3 = generate_web3_client(Some(max_resp), None)
+        .map_err(|e| format!("generate_web3_client failed: {}", e))?;
+    let signed_tx = sign_transfer_erc20_internal(
+        w3.clone(),
+        token_addr,
+        to_addr,
+        value,
+        tx_count,
+        gas_price
+    ).await?;
+    Ok(format!("0x{}", hex::encode(signed_tx.raw_transaction.0)))
+}
+
+#[update]
+async fn transfer_erc20(token_addr: String, to_addr: String, value: u64, tx_count: Option<u128>, gas_price: Option<u128>, max_resp: u64) -> Result<String, String> {
+    let w3 = generate_web3_client(Some(max_resp), None)
+        .map_err(|e| format!("generate_web3_client failed: {}", e))?;
+    let signed_tx = sign_transfer_erc20_internal(
+        w3.clone(),
+        token_addr,
+        to_addr,
+        value,
+        tx_count,
+        gas_price
+    ).await?;
+    match w3.eth().send_raw_transaction(signed_tx.raw_transaction).await {
+        Ok(v) => Ok(format!("0x{}", hex::encode(v))),
+        Err(msg) => Err(format!("send_raw_transaction failed: {}", msg))
+    }
+}
+
+async fn sign_transfer_erc20_internal(w3: Web3<ICHttp>, token_addr: String, to_addr: String, value: u64, tx_count: Option<u128>, gas_price: Option<u128>) -> Result<SignedTransaction, String> {
+    let to_addr = Address::from_str(&to_addr).unwrap();
+    sign_tx(
+        w3,
+        &token_addr,
+        ERC20_ABI,
+        &"transfer",
+        (to_addr, value,),
+        tx_count,
+        gas_price
+    ).await
+}
+
+async fn sign_tx(
+    w3: Web3<ICHttp>,
+    contract_addr: &str,
+    abi: &[u8],
+    func: &str,
+    params: impl Tokenize,
+    tx_count: Option<u128>,
+    gas_price: Option<u128>,
+) -> Result<SignedTransaction, String> {
+    let contract = generate_contract_client(w3.clone(), contract_addr, abi)
+        .map_err(|e| format!("generate_contract_client failed: {}", e))?;
+    let canister_addr = get_eth_addr(None, None, KN_IN_LOCAL.to_string()).await
+        .map_err(|e| format!("get_eth_addr failed: {}", e))?;
+
+    let tx_count = match tx_count {
+        Some(v) => U256::from(v),
+        None => w3.eth()
+            .transaction_count(canister_addr, None)
+            .await
+            .map_err(|e| format!("get tx count error: {}", e))?
+    };
+    let gas_price = match gas_price {
+        Some(v) => U256::from(v),
+        None => w3.eth()
+            .gas_price()
+            .await
+            .map_err(|e| format!("get gas_price error: {}", e))?
+    };
+    let options = Options::with(|op| {
+        op.nonce = Some(tx_count);
+        op.gas_price = Some(gas_price);
+        op.transaction_type = Some(U64::from(2)) // EIP1559_TX_ID
+    });
+
+    match contract.sign(
+        func,
+        params,
+        options,
+        hex::encode(canister_addr),
+        KeyInfo { derivation_path: vec![default_derivation_key()], key_name: KN_IN_LOCAL.to_string() },
+        CHAIN_ID
+    ).await {
+        Ok(v) => Ok(v),
+        Err(msg) => Err(format!("sign failed: {}", msg))
+    }
 }
--- various/rust/cycle_checkers_for_prod/src/measurer/src/utils.rs
+++ various/rust/cycle_checkers_for_prod/src/measurer/src/utils.rs
@@ -1,5 +1,7 @@
+use std::str::FromStr;
+
 use candid::Principal;
-use ic_web3::{types::Address, ic::{get_public_key as get_public_key_internal, pubkey_to_address as pubkey_to_address_internal }, transports::ICHttp, Web3};
+use ic_web3::{types::Address, ic::{get_public_key as get_public_key_internal, pubkey_to_address as pubkey_to_address_internal }, transports::ICHttp, Web3, contract::Contract};
 
 pub const KN_IN_LOCAL: &str = "dfx_test_key";
 
@@ -24,6 +26,15 @@ pub fn generate_web3_client(max_resp: Option<u64>, cycles: Option<u64>) -> Resul
         Ok(v) => Ok(Web3::new(v)),
         Err(e) => Err(e.to_string())
     }
+}
+
+pub fn generate_contract_client(w3: Web3<ICHttp>, contract_addr: &str, abi: &[u8]) -> Result<Contract<ICHttp>, String> {
+    let contract_address = Address::from_str(contract_addr).unwrap();
+    Contract::from_json(
+        w3.eth(),
+        contract_address,
+        abi
+    ).map_err(|e| format!("init contract failed: {}", e))
 }
 
 pub async fn get_public_key(
