dc3d19b1a91858ca0a7a5c7240eb42114b47f362
linnefromice <linnefromice@gmail.com>
2023-05-01 12:27:21 +0900

    update: script

--- with-uniswap/synchronizers_v2/scripts/src/index.ts
+++ with-uniswap/synchronizers_v2/scripts/src/index.ts
@@ -1,24 +1,53 @@
-console.log('Try npm run lint/fix!');
+import { ethers, providers } from "ethers"
 
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+const URL = "https://eth-mainnet.g.alchemy.com/v2/JVUDgQSB0r-3HhohPCod6uBy_Zx8WEdy";
+const MINITE = 60;
+const HOUR = 60 * MINITE;
+const DAY = 24 * HOUR;
+const MONTH = 30 * DAY;
 
-const trailing = 'Semicolon';
+const BLOCK_GENERATED_SEC = 15
+const BLOCK_COUNT_PER_MONTH = MONTH / BLOCK_GENERATED_SEC
 
-const why = 'am I tabbed?';
-
-export function doSomeStuff(
-  withThis: string,
-  andThat: string,
-  andThose: string[]
-) {
-  //function on one line
-  if (!andThose.length) {
-    return false;
-  }
-  console.log(withThis);
-  console.log(andThat);
-  console.dir(andThose);
-  return;
+// Get the latest block number
+const getLatestBlockNumber = async (provider: providers.JsonRpcProvider) => {
+  const blockNumber = await provider.getBlockNumber();
+  // console.log(`Latest block number: ${blockNumber}`);
+  return blockNumber;
 }
-// TODO: more examples
+
+// Get a block by its number
+const getBlockByNumber = async (provider: providers.JsonRpcProvider, blockNumber: number) => {
+  const block = await provider.getBlock(blockNumber);
+  // console.log(`Block number ${blockNumber}:`, block);
+  return block
+}
+
+const logBlockTimestamp = (block: providers.Block) => console.log(`${block.number}: ${new Date(block.timestamp * 1000).toISOString()} (${block.timestamp})`)
+
+const main = async () => {
+  const now = Math.floor(new Date().getTime() / 1000)
+  const provider = new ethers.providers.JsonRpcProvider(URL);
+
+  const latestBlockNumber = await getLatestBlockNumber(provider);
+  // const block0 = await getBlockByNumber(provider, 0);
+  // console.log(`0: ${new Date(block0.timestamp * 1000).toString()} (${block0.timestamp})`)
+  // const block1 = await getBlockByNumber(provider, 1);
+  // console.log(`1: ${new Date(block1.timestamp * 1000).toString()} (${block1.timestamp})`)
+  const latestBlock = await getBlockByNumber(provider, latestBlockNumber);
+  console.log(`${latestBlockNumber}: ${new Date(latestBlock.timestamp * 1000).toString()} (${latestBlock.timestamp})`)
+  const baseBlockNumber = 13916168
+  console.log(`baseBlockNumber: ${baseBlockNumber}`)
+  logBlockTimestamp(await getBlockByNumber(provider, baseBlockNumber - 4))
+  logBlockTimestamp(await getBlockByNumber(provider, baseBlockNumber - 3))
+  logBlockTimestamp(await getBlockByNumber(provider, baseBlockNumber - 2))
+  logBlockTimestamp(await getBlockByNumber(provider, baseBlockNumber - 1))
+  logBlockTimestamp(await getBlockByNumber(provider, baseBlockNumber))
+}
+
+main()
+  .then(() => console.log("DONE"))
+  .catch((err: any) => {
+    console.log("FAILED")
+    console.error(err)
+  })
