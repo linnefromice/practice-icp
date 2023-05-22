c096f008ca16122c9704689638d54b73ccdbff26
linnefromice <linnefromice@gmail.com>
2023-05-22 13:10:39 +0900

    add: macro to generate vec/map state & getter/setter

--- ic-web3-macros/examples/greet.rs
+++ ic-web3-macros/examples/greet.rs
@@ -1,4 +1,5 @@
 use ic_web3_macros::cross_canister_call_func;
+use candid::Principal;
 
 type CallCanisterArgs = (String, String);
 type CallCanisterResponse = Result<String, String>;
--- ic-web3-macros/src/lib.rs
+++ ic-web3-macros/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use syn::{parse::Parser, Type};
+use syn::{parse::Parser, Type, LitStr, parse_macro_input};
 use quote::{quote, format_ident};
 
 #[proc_macro]
@@ -76,6 +76,16 @@ pub fn manage_single_state(input: TokenStream) -> TokenStream {
     let get_ident = proc_macro2::Ident::new(&format!("get_{}", var_name), proc_macro2::Span::call_site());
     let set_ident = proc_macro2::Ident::new(&format!("set_{}", var_name), proc_macro2::Span::call_site());
 
+    let output = generate_single_state(var_type, var_init, var_ident, get_ident, set_ident);
+    output.into()
+}
+fn generate_single_state(
+    var_type: Type,
+    var_init: proc_macro2::TokenStream,
+    var_ident: proc_macro2::Ident,
+    get_ident: proc_macro2::Ident,
+    set_ident: proc_macro2::Ident,
+) -> proc_macro2::TokenStream {
     let output = quote! {
         thread_local! {
             static #var_ident: std::cell::RefCell<#var_type> = std::cell::RefCell::new(#var_init);
@@ -89,5 +99,92 @@ pub fn manage_single_state(input: TokenStream) -> TokenStream {
             #var_ident.with(|state| *state.borrow_mut() = value);
         }
     };
-    output.into()
+    output
+}
+
+struct ManageVecArgs {
+    name: LitStr,
+    ty: Type,
+}
+impl syn::parse::Parse for ManageVecArgs {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let name = input.parse()?;
+        input.parse::<syn::Token![,]>()?;
+        let ty = input.parse()?;
+        Ok(ManageVecArgs { name, ty })
+    }
+}
+#[proc_macro]
+pub fn manage_vec_state(input: TokenStream) -> TokenStream {
+    let ManageVecArgs { name, ty } = parse_macro_input!(input as ManageVecArgs);
+
+    let state_name = syn::Ident::new(&name.value(), name.span());
+    let get_vec_func = syn::Ident::new(&format!("get_{}s", state_name), name.span());
+    let get_elem_func = syn::Ident::new(&format!("get_{}", state_name), name.span());
+    let set_elem_func = syn::Ident::new(&format!("set_{}", state_name), name.span());
+
+    let expanded = quote! {
+        thread_local! {
+            static #state_name: std::cell::RefCell<Vec<#ty>> = std::cell::RefCell::new(Vec::new());
+        }
+
+        pub fn #get_vec_func() -> Vec<#ty> {
+            #state_name.with(|state| state.borrow().clone())
+        }
+
+        pub fn #get_elem_func(idx: usize) -> #ty {
+            #state_name.with(|state| state.borrow()[idx].clone())
+        }
+
+        pub fn #set_elem_func(value: #ty) {
+            #state_name.with(|state| state.borrow_mut().push(value));
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
+struct ManageMapArgs {
+    name: LitStr,
+    key_ty: Type,
+    val_ty: Type,
+}
+impl syn::parse::Parse for ManageMapArgs {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let name = input.parse()?;
+        input.parse::<syn::Token![,]>()?;
+        let key_ty = input.parse()?;
+        input.parse::<syn::Token![,]>()?;
+        let val_ty = input.parse()?;
+        Ok(ManageMapArgs { name, key_ty, val_ty })
+    }
+}
+#[proc_macro]
+pub fn manage_map_state(input: TokenStream) -> TokenStream {
+    let ManageMapArgs { name, key_ty, val_ty } = parse_macro_input!(input as ManageMapArgs);
+
+    let state_name = syn::Ident::new(&name.value(), name.span());
+    let get_len_func = syn::Ident::new(&format!("{}s_len", state_name), name.span());
+    let get_elem_func = syn::Ident::new(&format!("get_{}", state_name), name.span());
+    let set_elem_func = syn::Ident::new(&format!("set_{}", state_name), name.span());
+
+    let expanded = quote! {
+        thread_local! {
+            static #state_name: std::cell::RefCell<std::collections::HashMap<#key_ty, #val_ty>> = std::cell::RefCell::new(std::collections::HashMap::new());
+        }
+
+        pub fn #get_len_func() -> usize {
+            #state_name.with(|state| state.borrow().len())
+        }
+
+        pub fn #get_elem_func(key: #key_ty) -> #val_ty {
+            #state_name.with(|state| state.borrow().get(&key).cloned().expect("key not found"))
+        }
+
+        pub fn #set_elem_func(key: #key_ty, value: #val_ty) {
+            #state_name.with(|state| state.borrow_mut().insert(key, value));
+        }
+    };
+
+    TokenStream::from(expanded)
 }
