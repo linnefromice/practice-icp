506fbd30640afe3a4b415f0c05ae10eb66685b28
linnefromice <linnefromice@gmail.com>
2023-06-17 23:46:13 +0900

    update: e2e - add http_outcall canisters (erc20, UniswapV3Pool)

--- simple_e2e/README.md
+++ simple_e2e/README.md
@@ -8,10 +8,17 @@ dfx stop && dfx start --clean --background && dfx canister create --all && dfx b
 # confirmation
 dfx deploy
 
-dfx canister call counter_motoko label '()'
-dfx canister call counter_rust label '()'
+dfx canister call counter_motoko name '()'
 dfx canister call counter_motoko get '()'
+dfx canister call counter_rust name '()'
 dfx canister call counter_rust get '()'
+dfx canister call http_outcall_erc20 name '()'
+dfx canister call http_outcall_erc20 symbol '()'
+dfx canister call http_outcall_erc20 total_supply '()'
+dfx canister call http_outcall_pool token0 '()'
+dfx canister call http_outcall_pool token1 '()'
+dfx canister call http_outcall_pool fee '()'
+dfx canister call http_outcall_pool slot0 '()'
 
-cd tests && yarn test --test-timeout=20000
+cd tests && yarn test --test-timeout=30000
 ```
--- simple_e2e/scripts/build.sh
+++ simple_e2e/scripts/build.sh
@@ -19,3 +19,15 @@ echo "Building counter_rust"
 cp -f src/counter_rust/counter_rust.did artifacts/counter_rust.did
 cargo build --target wasm32-unknown-unknown --release --package counter_rust
 ic-wasm target/wasm32-unknown-unknown/release/counter_rust.wasm -o artifacts/counter_rust.wasm shrink
+
+# http_outcall_erc20
+echo "Building http_outcall_erc20"
+cp -f src/http_outcall_erc20/http_outcall_erc20.did artifacts/http_outcall_erc20.did
+cargo build --target wasm32-unknown-unknown --release --package http_outcall_erc20
+ic-wasm target/wasm32-unknown-unknown/release/http_outcall_erc20.wasm -o artifacts/http_outcall_erc20.wasm shrink
+
+# http_outcall_pool
+echo "Building http_outcall_pool"
+cp -f src/http_outcall_pool/http_outcall_pool.did artifacts/http_outcall_pool.did
+cargo build --target wasm32-unknown-unknown --release --package http_outcall_pool
+ic-wasm target/wasm32-unknown-unknown/release/http_outcall_pool.wasm -o artifacts/http_outcall_pool.wasm shrink
