5e1d6c78017af9138f005bee3fecc218745fd11b
linnefromice <linnefromice@gmail.com>
2023-07-06 13:34:16 +0900

    update

---
+++ use_stable_structure/src/basic_example_measurement/src/lib.rs
@@ -0,0 +1,85 @@
+use std::cell::RefCell;
+
+use chainsight_cdk_macros::did_export;
+use ic_stable_structures::{
+    memory_manager::{MemoryId, MemoryManager, VirtualMemory},
+    DefaultMemoryImpl,
+};
+use ic_stable_structures::{StableVec};
+
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+
+thread_local! {
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+
+    static VEC: RefCell<StableVec<u128, Memory>> = RefCell::new(
+        StableVec::init(
+            MEMORY_MANAGER.with(|mm| mm.borrow().get(MemoryId::new(0))),
+        ).unwrap()
+    );
+}
+
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+fn greet(name: String) -> String {
+    format!("Hello, {}!", name)
+}
+
+//// Vec
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+fn get_vec_val(idx: u64) -> (Option<u128>, u64) {
+    let start = ic_cdk::api::instruction_counter();
+    let res = VEC.with(|mem| mem.borrow().get(idx));
+    (res, ic_cdk::api::instruction_counter() - start)
+}
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+fn get_vec_vals_len() -> (u64, u64) {
+    let start = ic_cdk::api::instruction_counter();
+    let res = VEC.with(|mem| mem.borrow().len());
+    (res, ic_cdk::api::instruction_counter() - start)
+}
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+fn get_last_vec_val() -> (Option<u128>, u64) {
+    let start = ic_cdk::api::instruction_counter();
+    let res = VEC.with(|mem| {
+        let borrowed_mem = mem.borrow();
+        let len = borrowed_mem.len();
+        borrowed_mem.get(len - 1)
+    });
+    (res, ic_cdk::api::instruction_counter() - start)
+}
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+fn get_top_vec_vals(n: u64) -> (Vec<u128>, u64) {
+    let start = ic_cdk::api::instruction_counter();
+    let res = VEC.with(|mem| {
+        let borrowed_mem = mem.borrow();
+        let start_index = borrowed_mem.len().saturating_sub(n);
+        let mut vec_var: Vec<u128> = borrowed_mem.iter().collect();
+        vec_var.split_off(start_index as usize).iter().rev().cloned().collect()
+    });
+    (res, ic_cdk::api::instruction_counter() - start)
+}
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+fn get_vec() -> (Vec<u128>, u64) {
+    let start = ic_cdk::api::instruction_counter();
+    let res = VEC.with(|mem| mem.borrow().iter().collect());
+    (res, ic_cdk::api::instruction_counter() - start)
+}
+#[ic_cdk::update]
+#[candid::candid_method(update)]
+fn add_vec_val(value: u128) -> (Result<(), String>, u64) {
+    let start = ic_cdk::api::instruction_counter();
+    let res = VEC.with(|vec| vec.borrow_mut().push(&value));
+    (
+        res.map_err(|e| format!("{:?}", e)),
+        ic_cdk::api::instruction_counter() - start
+    )
+}
+
+did_export!("basic_example_measurement");
