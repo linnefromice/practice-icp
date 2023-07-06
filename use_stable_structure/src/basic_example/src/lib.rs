44e5e0d72128b9e88402fd2b9d6640d4ec37fc5d
linnefromice <linnefromice@gmail.com>
2023-07-06 12:50:59 +0900

    update

--- use_stable_structure/src/basic_example/src/lib.rs
+++ use_stable_structure/src/basic_example/src/lib.rs
@@ -5,7 +5,7 @@ use ic_stable_structures::{
     memory_manager::{MemoryId, MemoryManager, VirtualMemory},
     DefaultMemoryImpl,
 };
-use ic_stable_structures::{StableBTreeMap, StableVec};
+use ic_stable_structures::{StableCell, StableBTreeMap, StableVec};
 
 type Memory = VirtualMemory<DefaultMemoryImpl>;
 
@@ -13,15 +13,22 @@ thread_local! {
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
         RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
 
+    static CELL: RefCell<StableCell<u128, Memory>> = RefCell::new(
+        StableCell::init(
+            MEMORY_MANAGER.with(|mm| mm.borrow().get(MemoryId::new(0))),
+            Default::default()
+        ).unwrap()
+    );
+
     static MAP: RefCell<StableBTreeMap<u128, u128, Memory>> = RefCell::new(
         StableBTreeMap::init(
-            MEMORY_MANAGER.with(|mm| mm.borrow().get(MemoryId::new(0))),
+            MEMORY_MANAGER.with(|mm| mm.borrow().get(MemoryId::new(1))),
         )
     );
 
     static VEC: RefCell<StableVec<u128, Memory>> = RefCell::new(
         StableVec::init(
-            MEMORY_MANAGER.with(|mm| mm.borrow().get(MemoryId::new(1))),
+            MEMORY_MANAGER.with(|mm| mm.borrow().get(MemoryId::new(2))),
         ).unwrap()
     );
 }
@@ -30,6 +37,19 @@ thread_local! {
 #[candid::candid_method(query)]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
+}
+
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+fn get_cell() -> u128 {
+    CELL.with(|cell| cell.borrow().get().clone())
+}
+
+#[ic_cdk::update]
+#[candid::candid_method(update)]
+fn set_cell(val: u128) -> Result<(), String> {
+    let res = CELL.with(|cell| cell.borrow_mut().set(val));
+    res.map(|_| ()).map_err(|e| format!("{:?}", e))
 }
 
 #[ic_cdk::query]
