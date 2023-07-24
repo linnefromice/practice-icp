011dfa0de2e87b9f32ea68c0182bd4a4a7ac1dc0
linnefromice <linnefromice@gmail.com>
2023-07-24 13:40:15 +0900

    update

--- ws_proxy/with_proxy_1/src/callee_impl/src/lib.rs
+++ ws_proxy/with_proxy_1/src/callee_impl/src/lib.rs
@@ -1,4 +1,30 @@
+use candid::{Decode, Encode};
+use chainsight_cdk_macros::{did_export, init_in, prepare_stable_structure, stable_memory_for_vec, StableMemoryStorable};
+
+init_in!();
+prepare_stable_structure!();
+stable_memory_for_vec!("snapshot", Snapshot, 0, true);
+#[derive(
+    Debug,
+    Clone,
+    candid :: CandidType,
+    candid :: Deserialize,
+    serde :: Serialize,
+    StableMemoryStorable,
+)]
+#[stable_mem_storable_opts(max_size = 10000, is_fixed_size = false)]
+pub struct Snapshot(pub String);
+
+#[candid::candid_method(query)]
 #[ic_cdk::query]
 fn greet(name: String) -> String {
-    format!("Hello, {}! I'm callee.", name)
+    format!("Hello, {}! I'm caller.", name)
 }
+
+#[candid::candid_method(update)]
+#[ic_cdk::update]
+fn insert_snapshot(value: Snapshot) -> Result<(), String> {
+    add_snapshot(value)
+}
+
+did_export!("callee_impl");
--- ws_proxy/with_proxy_1/src/caller_impl/src/lib.rs
+++ ws_proxy/with_proxy_1/src/caller_impl/src/lib.rs
@@ -1,4 +1,33 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}! I'm caller.", name)
+use candid::{Decode, Encode};
+use chainsight_cdk_macros::did_export;
+
+#[candid::candid_method(update)]
+#[ic_cdk::update]
+async fn call_to_impl(
+    canister_id: candid::Principal,
+    idx: u64,
+) -> Result<String, String> {
+    let res = ic_cdk::api::call::call::<_, (String,)>(
+        canister_id,
+        "get_snapshot",
+        (idx,)
+    ).await.map_err(|e| format!("call error: {:?}", e))?;
+
+    Ok(res.0)
 }
+
+#[candid::candid_method(update)]
+#[ic_cdk::update]
+async fn call_to_impl_last(
+    canister_id: candid::Principal,
+) -> Result<String, String> {
+    let res = ic_cdk::api::call::call::<_, (String,)>(
+        canister_id,
+        "get_last_snapshot",
+        ()
+    ).await.map_err(|e| format!("call error: {:?}", e))?;
+
+    Ok(res.0)
+}
+
+did_export!("caller_impl");
