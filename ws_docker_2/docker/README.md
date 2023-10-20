8306451faf96149e17104bf6315f9d0504baa169
linnefromice <linnefromice@gmail.com>
2023-10-20 14:41:39 +0900

    update: host

--- ws_docker_2/docker/README.md
+++ ws_docker_2/docker/README.md
@@ -5,6 +5,13 @@
 docker build -f Dockerfile.base -t dfx_hardhat_node_base . --progress=plain && docker build -f Dockerfile.pj -t dfx_hardhat_node . --progress=plain
 docker run -it --name dfx_hardhat_node -p 18545:18545 -p 14943:14943 --rm dfx_hardhat_node /bin/bash
 
+docker run -it \
+  --name dfx_hardhat_node \
+  -p 18545:18545 -p 14943:14943 \
+  -v $PWD/.artifacts:/workspace/artifacts \
+  -v $PWD/.outputs:/workspace/outputs \
+  --rm dfx_hardhat_node /bin/bash
+
 ## With mount
 rm -rf ./.artifacts ./.outputs && cp -rp ./artifacts_csx ./.artifacts && mkdir ./.outputs
 docker run -t \
--- ws_docker_2/docker/workspace_pj/bootstrap.sh
+++ ws_docker_2/docker/workspace_pj/bootstrap.sh
@@ -1,31 +1,19 @@
 #!/usr/bin/env bash
-echo "> ls -l"
-ls -l
-## for dfx
-echo "> ls -l artifacts"
-ls -l artifacts
-echo "> cp -rp artifacts ws_dfx"
 cp -rp artifacts ws_dfx
-echo "> cd ws_dfx"
 cd ws_dfx
-echo "> pwd"
-pwd
-echo "> dfx start --background --clean"
+
+# dfx
 dfx start --background --clean
-echo "> dfx canister create --all"
 dfx canister create --all
-echo "> dfx build"
 dfx build
-echo "> dfx canister install --all"
 dfx canister install --all
-echo "> check dashboard url (dfx deploy)"
+## check dashboard url (dfx deploy)
 dfx deploy
-echo "> collect deliverables from deployments"
+## collect deliverables from deployments
 mkdir -p ../outputs/.dfx
 cp -rp .env ../outputs
 cp -rp .dfx/local ../outputs/.dfx
-## for hardhat
-echo "> cd .."
+
+# hardhat
 cd ..
-echo "> yarn hardhat node --port 18545"
 yarn hardhat node --port 18545
