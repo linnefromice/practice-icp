94eff9417d389d8ff68a3e2602deb15386b4fb54
linnefromice <linnefromice@gmail.com>
2023-10-24 14:50:29 +0900

    update

--- ws_candid_parser/src/main.rs
+++ ws_candid_parser/src/main.rs
@@ -4,17 +4,30 @@ use anyhow::Result;
 use candid::pretty_check_file;
 
 fn main() -> Result<()> {
-    let candid_path = "assets/sample.did";
-    let (env, actor) = pretty_check_file(Path::new(candid_path))?;
-    let config = candid::bindings::rust::Config::new();
+    Ok(())
+}
 
-    let result = candid::bindings::rust::compile(&config, &env, &None);
-    println!("{}", result);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    if let Some(actor) = actor {
-        let result = candid::bindings::rust::compile(&config, &env, &Some(actor));
-        println!("{}", result);
+    use insta::assert_snapshot;
+
+    #[test]
+    fn test_env_from_file() {
+        let candid_path = "assets/sample.did";
+        let (env, _) = pretty_check_file(Path::new(candid_path)).unwrap();
+        let config = candid::bindings::rust::Config::new();
+        let result = candid::bindings::rust::compile(&config, &env, &None);
+        assert_snapshot!(result);
     }
 
-    Ok(())
+    #[test]
+    fn test_actor_from_file() {
+        let candid_path = "assets/sample.did";
+        let (env, actor) = pretty_check_file(Path::new(candid_path)).unwrap();
+        let config = candid::bindings::rust::Config::new();
+        let result = candid::bindings::rust::compile(&config, &env, &Some(actor.unwrap()));
+        assert_snapshot!(result);
+    }
 }
