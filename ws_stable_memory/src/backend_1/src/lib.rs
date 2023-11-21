60a41089d4c1f360d5cf700ccaace72f32a374eb
linnefromice <linnefromice@gmail.com>
2023-11-21 17:17:01 +0900

    update

--- ws_stable_memory/src/backend_1/src/lib.rs
+++ ws_stable_memory/src/backend_1/src/lib.rs
@@ -1,27 +1,29 @@
 use std::{cell::RefCell, borrow::Cow};
 
 use candid::{CandidType, Deserialize, Decode, Encode};
-use ic_cdk_macros::init;
-use ic_stable_structures::{memory_manager::{VirtualMemory, MemoryManager, MemoryId}, DefaultMemoryImpl, Storable, BoundedStorable, Cell};
+use ic_cdk_macros::{init, pre_upgrade, post_upgrade};
+use ic_stable_structures::{memory_manager::{VirtualMemory, MemoryManager, MemoryId}, DefaultMemoryImpl, Storable, BoundedStorable, Cell, writer::Writer, Memory};
 
-type Memory = VirtualMemory<DefaultMemoryImpl>;
+type MemoryType = VirtualMemory<DefaultMemoryImpl>;
+
+const MEMORY_ID_FOR_UPGRADE: MemoryId = MemoryId::new(0);
 
 thread_local! {
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
         RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
     
     // stable memory
-    static PLAYERS: RefCell<ic_stable_structures::Vec<Player, Memory>> = RefCell::new(
+    static PLAYERS: RefCell<ic_stable_structures::Vec<Player, MemoryType>> = RefCell::new(
         ic_stable_structures::Vec::init(MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1)))).unwrap()
     );
-    static SUM_AGE: RefCell<Cell<u32, Memory>> = RefCell::new(Cell::init(MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(2))), 0).unwrap());
+    static SUM_AGE: RefCell<Cell<u32, MemoryType>> = RefCell::new(Cell::init(MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(2))), 0).unwrap());
 
     // heap
     static LENGTH: RefCell<usize> = RefCell::new(0);
     static LATEST_PLAYER: std::cell::RefCell<Option<Player>> = std::cell::RefCell::new(None);
 }
 
-#[derive(CandidType, Deserialize, Clone, Debug, PartialEq)]
+#[derive(CandidType, serde::Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct Player {
     index: u32,
     name: String,
@@ -42,20 +44,20 @@ impl BoundedStorable for Player {
 
 #[ic_cdk::query]
 #[candid::candid_method(query)]
-fn get_player(index: u64) -> Player {
-    PLAYERS.with(|p| p.borrow().get(index).unwrap())
+fn get_player(index: u64) -> Option<Player> {
+    PLAYERS.with(|p| p.borrow().get(index)).clone()
 }
 
 #[ic_cdk::query]
 #[candid::candid_method(query)]
-fn get_latest_player_from_heap() -> Player {
-    LATEST_PLAYER.with(|l| l.borrow().clone().unwrap())
+fn get_latest_player_from_heap() -> Option<Player> {
+    LATEST_PLAYER.with(|l| l.borrow().clone()).clone()
 }
 
 #[ic_cdk::query]
 #[candid::candid_method(query)]
-fn get_latest_player_from_stable() -> Player {
-    PLAYERS.with(|p| p.borrow().get(p.borrow().len() as u64 - 1).unwrap())
+fn get_latest_player_from_stable() -> Option<Player> {
+    PLAYERS.with(|p| p.borrow().get(p.borrow().len() as u64 - 1)).clone()
 }
 
 #[ic_cdk::query]
@@ -102,6 +104,45 @@ fn _add_player(name: String, age: u32) -> Player {
     player
 }
 
+pub fn get_upgrades_memory() -> MemoryType {
+    MEMORY_MANAGER.with(|m| m.borrow().get(MEMORY_ID_FOR_UPGRADE))
+}
+#[derive(serde::Serialize, Deserialize)]
+pub struct UpgradeState {
+    length: u64,
+    latest_player: Player,
+}
+#[pre_upgrade]
+fn pre_upgrade() {
+    let mut state_bytes = vec![];
+    ciborium::ser::into_writer(&UpgradeState {
+        length: get_players_len_from_heap(),
+        latest_player: get_latest_player_from_heap().unwrap(),
+    }, &mut state_bytes).unwrap();
+
+    let len = state_bytes.len() as u32;
+    let mut memory = get_upgrades_memory();
+    let mut writer = Writer::new(&mut memory, 0);
+    writer.write(&len.to_le_bytes()).unwrap();
+    writer.write(&state_bytes).unwrap()
+}
+#[post_upgrade]
+fn post_upgrade() {
+    let memory = get_upgrades_memory();
+
+    // Read the length of the state bytes.
+    let mut state_len_bytes = [0; 4];
+    memory.read(0, &mut state_len_bytes);
+    let state_len = u32::from_le_bytes(state_len_bytes) as usize;
+
+    // Read the bytes
+    let mut state_bytes = vec![0; state_len];
+    memory.read(4, &mut state_bytes);
+
+    let state: UpgradeState = ciborium::de::from_reader(&*state_bytes).expect("failed to decode state");
+    LENGTH.with(|l| *l.borrow_mut() = state.length as usize);
+    LATEST_PLAYER.with(|l| *l.borrow_mut() = Some(state.latest_player));
+}
 
 #[init]
 fn init() {
@@ -124,14 +165,31 @@ mod tests {
         assert_eq!(get_players_len_from_stable(), 1);
         assert_eq!(get_players_len_from_heap(), 1);
         let expected_latest_player = Player { index: 0, name: "Anonymous".to_string(), age: 99 };
-        assert_eq!(get_latest_player_from_heap(), expected_latest_player.clone());
-        assert_eq!(get_latest_player_from_stable(), expected_latest_player.clone());
+        assert_eq!(get_latest_player_from_heap().unwrap(), expected_latest_player.clone());
+        assert_eq!(get_latest_player_from_stable().unwrap(), expected_latest_player.clone());
 
         let player = add_player("Alice".to_string(), 20);
-        assert_eq!(get_latest_player_from_heap(), player.clone());
-        assert_eq!(get_latest_player_from_stable(), player.clone());
+        assert_eq!(get_latest_player_from_heap().unwrap(), player.clone());
+        assert_eq!(get_latest_player_from_stable().unwrap(), player.clone());
 
         assert_eq!(get_players_len_from_heap(), 2);
         assert_eq!(get_players_len_from_stable(), 2);
+    }
+
+    #[test]
+    fn use_cbor() {
+        let _ = init();
+        let length = get_players_len_from_heap();
+        let latest_player = get_latest_player_from_heap().unwrap();
+
+        let mut state_bytes = vec![];
+        ciborium::ser::into_writer(&UpgradeState {
+            length,
+            latest_player: latest_player.clone(),
+        }, &mut state_bytes).unwrap();
+
+        let state: UpgradeState = ciborium::de::from_reader(&*state_bytes).expect("failed to decode state");
+        assert_eq!(state.length, length);
+        assert_eq!(state.latest_player, latest_player);
     }
 }
