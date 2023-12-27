1d8b595892194b8a3986a7114be76e40f427096d
linnefromice <linnefromice@gmail.com>
2023-12-27 22:05:26 +0900

    update

--- ws_agent/agent_1/src/main.rs
+++ ws_agent/agent_1/src/main.rs
@@ -1,4 +1,5 @@
 use core::fmt;
+use std::process::Command;
 
 use candid::{Principal, Encode, Decode, CandidType};
 
@@ -31,7 +32,7 @@ impl fmt::Display for Environment {
     }
 }
 impl Environment {
-    fn get_registry_canister_id(&self) -> Principal {
+    fn _get_registry_canister_id(&self) -> Principal {
         let principal_str = match self {
             Environment::Local => "uh54g-lyaaa-aaaal-achca-cai",
             Environment::IC => "xd2oy-taaaa-aaaal-qcnva-cai",
@@ -72,16 +73,33 @@ async fn main() {
     
     let component_id = Principal::from_text(canister_id).unwrap();
     let proxy = get_proxy_from_component(&agent, &component_id).await;
-    let vault = vault_from_proxy(&agent, &proxy).await;
-    let db = db_from_proxy(&agent, &proxy).await;
     println!("proxy: {}", proxy);
+    let vault = vault_from_proxy(&agent, &proxy).await;
     println!("vault: {}", vault);
+    let db = db_from_proxy(&agent, &proxy).await;
     println!("db: {}", db);
+
+    let wallet = get_wallet().unwrap();
+
+    println!("deleting db...");
+    delete_canister(&db, &wallet);
+    println!("deleted db...");
+    println!("deleting vault...");
+    delete_canister(&vault, &wallet);
+    println!("deleted vault...");
+    println!("deleting proxy...");
+    delete_canister(&proxy, &wallet);
+    println!("deleted proxy...");
+    println!("deleting component...");
+    delete_canister(&component_id, &wallet);
+    println!("deleted component...");
+
 }
 
 fn agent(url: &str) -> ic_agent::Agent {
     let agent = ic_agent::Agent::builder()
         .with_url(url)
+        .with_verify_query_signatures(false)
         .build()
         .unwrap();
     agent
@@ -123,7 +141,7 @@ async fn db_from_proxy(agent: &ic_agent::Agent, principal: &Principal) -> Princi
     Decode!(res.as_slice(), Principal).unwrap()
 }
 
-async fn scan_canisters_from_registry(agent: &ic_agent::Agent, principal: &Principal) -> Vec<Canister> {
+async fn _scan_canisters_from_registry(agent: &ic_agent::Agent, principal: &Principal) -> Vec<Canister> {
     let res = agent.update(
         &principal,
         "scanCanisters",
@@ -133,4 +151,47 @@ async fn scan_canisters_from_registry(agent: &ic_agent::Agent, principal: &Princ
     .await
     .unwrap();
     Decode!(res.as_slice(), Vec<Canister>).unwrap()
+}
+
+fn get_wallet() -> Result<Principal, String> {
+    // TODO: consider --ic
+    let output = Command::new("dfx")
+        .args([
+            "identity",
+            "get-wallet"
+        ])
+        .output()
+        .expect("failed to execute process");
+    if output.status.success() {
+        let msg = std::str::from_utf8(&output.stdout).unwrap_or("failed to parse stdout");
+        Ok(Principal::from_text(msg.replace("\n", "")).unwrap())
+    } else {
+        let msg = std::str::from_utf8(&output.stderr).unwrap_or("failed to parse stderr");
+        Err(msg.to_string())
+    }
+}
+
+fn delete_canister(target: &Principal, wallet: &Principal) {
+    // TODO: consider --ic
+    let output = Command::new("dfx")
+        .args([
+            "canister",
+            "delete",
+            "--wallet",
+            &wallet.to_text(),
+            "--yes",
+            &target.to_text(),
+        ])
+        .output()
+        .expect("failed to execute process");
+    println!("> stdout");
+    println!(
+        "{}",
+        std::str::from_utf8(&output.stdout).unwrap_or("failed to parse stdout")
+    );
+    println!("> stderr");
+    println!(
+        "{}",
+        std::str::from_utf8(&output.stderr).unwrap_or("failed to parse stderr")
+    );
 }
