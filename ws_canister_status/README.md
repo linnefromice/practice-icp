fe91fdadd1ee4271991e03e5a00297832649fa61
linnefromice <linnefromice@gmail.com>
2024-01-05 17:09:29 +0900

    update

--- ws_canister_status/README.md
+++ ws_canister_status/README.md
@@ -1,1 +1,12 @@
 # ws_canister_status
+
+```bash
+dfx start --background --clean
+dfx deploy
+dfx canister update-settings --add-controller $(dfx canister id backend_1) backend_2
+dfx canister update-settings --add-controller $(dfx canister id backend_1) backend_3
+
+dfx canister call backend_1 call_status_self
+dfx canister call backend_1 call_status "$(dfx canister id backend_2)"
+dfx canister call backend_1 call_status "$(dfx canister id backend_3)"
+```
--- ws_canister_status/src/backend/src/lib.rs
+++ ws_canister_status/src/backend/src/lib.rs
@@ -1,10 +1,46 @@
+use candid::Principal;
+use ic_cdk::api::{management_canister::{main::{canister_status, CanisterStatusResponse}, provisional::CanisterIdRecord}, call::CallResult};
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
 
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+async fn query_status_self() -> CanisterStatusResponse {
+    call_canister_status(CanisterIdRecord { canister_id: ic_cdk::api::id() }).await.unwrap().0
+}
+
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+async fn query_status(id: String) -> CanisterStatusResponse {
+    let id = Principal::from_text(id).unwrap();
+    call_canister_status(CanisterIdRecord { canister_id: id }).await.unwrap().0
+}
+
+#[ic_cdk::update]
+#[candid::candid_method(update)]
+async fn call_status_self() -> CanisterStatusResponse {
+    call_canister_status(CanisterIdRecord { canister_id: ic_cdk::api::id() }).await.unwrap().0
+}
+
+#[ic_cdk::update]
+#[candid::candid_method(update)]
+async fn call_status(id: String) -> CanisterStatusResponse {
+    let id = Principal::from_text(id).unwrap();
+    call_canister_status(CanisterIdRecord { canister_id: id }).await.unwrap().0
+}
+
+async fn call_canister_status(id_record: CanisterIdRecord) -> CallResult<(CanisterStatusResponse,)> {
+    canister_status(id_record)
+        .await
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     candid::export_service!();
 
     #[test]
