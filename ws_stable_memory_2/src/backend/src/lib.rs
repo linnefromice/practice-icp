e824035464a51057bf1dbe26050f3f579fcbd09c
linnefromice <linnefromice@gmail.com>
2024-01-10 12:18:55 +0900

    update

--- ws_stable_memory_2/src/backend/src/lib.rs
+++ ws_stable_memory_2/src/backend/src/lib.rs
@@ -1,10 +1,21 @@
 use std::cell::RefCell;
 
+use ic_stable_structures::{memory_manager::{MemoryManager, VirtualMemory, MemoryId}, DefaultMemoryImpl};
+
 mod types;
 
+type MemoryType = VirtualMemory<DefaultMemoryImpl>;
+
 thread_local! {
+    // heap
     static HEAP_DATUM: RefCell<Option<types::Snapshot>> = RefCell::new(None);
     static HEAP_DATA: RefCell<Vec<types::Snapshot>> = RefCell::new(Vec::new());
+
+    // stable memory
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+    static STABLE_DATUM: RefCell<ic_stable_structures::Cell<types::Snapshot, MemoryType>> = RefCell::new(ic_stable_structures::Cell::init(MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1))), types::Snapshot::default()).unwrap());
+    static STABLE_DATA: RefCell<ic_stable_structures::Vec<types::Snapshot, MemoryType>> = RefCell::new(ic_stable_structures::Vec::init(MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(2)))).unwrap());
 }
 
 #[ic_cdk::query]
@@ -15,14 +26,14 @@ fn greet(name: String) -> String {
 
 #[ic_cdk::query]
 #[candid::candid_method(query)]
-fn get_data_by_json() -> types::SnapshotValue {
-    let raw = get_raw_data_by_json();
+fn raw_get_data() -> types::SnapshotValue {
+    let raw = raw_data_by_json();
     serde_json::from_str(&raw).unwrap()
 }
 #[ic_cdk::query]
 #[candid::candid_method(query)]
 fn get_snapshot_by_json() -> types::Snapshot {
-    let value = get_data_by_json();
+    let value = raw_get_data();
     types::Snapshot {
         value,
         timestamp: 0,
@@ -30,22 +41,22 @@ fn get_snapshot_by_json() -> types::Snapshot {
 }
 #[ic_cdk::query]
 #[candid::candid_method(query)]
-fn get_raw_data_by_json() -> String {
+fn raw_data_by_json() -> String {
     include_str!("../../v3pool.json").to_string()
 }
 
+// heap
 #[ic_cdk::query]
 #[candid::candid_method(query)]
-fn get_datum() -> Option<types::Snapshot> {
+fn heap_get_datum() -> Option<types::Snapshot> {
     let datum = HEAP_DATUM.with(|mem| {
         mem.borrow().clone()
     });
     datum
 }
-
 #[ic_cdk::query]
 #[candid::candid_method(query)]
-fn get_last_data() -> types::Snapshot {
+fn heap_get_last_data() -> types::Snapshot {
     let datum = HEAP_DATA.with(|mem| {
         let borrowed_mem = mem.borrow();
         let len = borrowed_mem.len();
@@ -54,10 +65,9 @@ fn get_last_data() -> types::Snapshot {
     });
     datum
 }
-
 #[ic_cdk::query]
 #[candid::candid_method(query)]
-fn get_selected_data(n: u64) -> types::Snapshot {
+fn heap_get_selected_data(n: u64) -> types::Snapshot {
     let datum = HEAP_DATA.with(|mem| {
         let borrowed_mem = mem.borrow();
         let res = borrowed_mem.get(n as usize);
@@ -65,7 +75,6 @@ fn get_selected_data(n: u64) -> types::Snapshot {
     });
     datum
 }
-
 #[ic_cdk::update]
 #[candid::candid_method(update)]
 fn add_datum() {
@@ -74,13 +83,59 @@ fn add_datum() {
         *mem.borrow_mut() = Some(datum.clone());
     });
 }
-
 #[ic_cdk::update]
 #[candid::candid_method(update)]
-fn add_data() {
+fn heap_add_data() {
     let datum = get_snapshot_by_json();
     HEAP_DATA.with(|mem| {
         mem.borrow_mut().push(datum);
+    });
+}
+
+// stable_memory
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+fn stable_get_datum() -> types::Snapshot {
+    let datum = STABLE_DATUM.with(|p| p.borrow().get().clone());
+    datum
+}
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+fn stable_get_last_data() -> types::Snapshot {
+    let datum = STABLE_DATA.with(|mem| {
+        let borrowed_mem = mem.borrow();
+        let len = borrowed_mem.len();
+        let res = borrowed_mem.get(len - 1);
+        res.unwrap().clone()
+    });
+    datum
+}
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+fn stable_get_selected_data(n: u64) -> types::Snapshot {
+    let datum = STABLE_DATA.with(|mem| {
+        let borrowed_mem = mem.borrow();
+        let res = borrowed_mem.get(n as u64);
+        res.unwrap().clone()
+    });
+    datum
+}
+#[ic_cdk::update]
+#[candid::candid_method(update)]
+fn stable_add_datum() {
+    let datum = get_snapshot_by_json();
+    STABLE_DATUM.with(|mem| {
+        mem.borrow_mut().set(datum.clone()).unwrap();
+    });
+}
+#[ic_cdk::update]
+#[candid::candid_method(update)]
+fn stable_add_data(is_default: bool) {
+    let datum = is_default
+        .then(|| types::Snapshot::default())
+        .unwrap_or_else(get_snapshot_by_json);
+    STABLE_DATA.with(|mem| {
+        mem.borrow_mut().push(&datum).unwrap();
     });
 }
 
--- ws_stable_memory_2/src/backend/src/types.rs
+++ ws_stable_memory_2/src/backend/src/types.rs
@@ -1,18 +1,34 @@
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, borrow::Cow};
 
-#[derive(Debug, Clone, candid::CandidType, candid::Deserialize, serde::Serialize)]
+use candid::{Decode, Encode};
+use ic_stable_structures::{Storable, BoundedStorable};
+
+#[derive(Debug, Clone, Default, candid::CandidType, candid::Deserialize, serde::Serialize)]
 pub struct Snapshot {
     pub value: SnapshotValue,
     pub timestamp: u64,
 }
+impl Storable for Snapshot {
+    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+}
+impl BoundedStorable for Snapshot {
+    const MAX_SIZE: u32 = 100;
+    const IS_FIXED_SIZE: bool = false;
+}
 
-#[derive(Debug, Clone, candid::CandidType, candid::Deserialize, serde::Serialize)]
+#[derive(Debug, Clone, Default, candid::CandidType, candid::Deserialize, serde::Serialize)]
 pub struct SnapshotValue {
     pub jsonrpc: String,
     pub id: String,
     pub result: ResultV3Pool,
 }
-#[derive(Debug, Clone, candid::CandidType, candid::Deserialize, serde::Serialize)]
+
+#[derive(Debug, Clone, Default, candid::CandidType, candid::Deserialize, serde::Serialize)]
 pub struct ResultV3Pool {
     pub address: String,
     pub token0: String,
@@ -23,7 +39,8 @@ pub struct ResultV3Pool {
     #[serde(deserialize_with = "from_ticks")]
     pub ticks: BTreeMap<String, Tick>
 }
-#[derive(Debug, Clone, candid::CandidType, candid::Deserialize, serde::Serialize)]
+
+#[derive(Debug, Clone, Default, candid::CandidType, candid::Deserialize, serde::Serialize)]
 pub struct Tick {
     pub liquidity_gross: String,
     pub liquidity_net: String,
