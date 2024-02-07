23ab34b994e0c6f39397fc7438afcb7a7fe2e14c
linnefromice <linnefromice@gmail.com>
2024-02-07 21:42:07 +0900

    update

--- ws_candid/candid_type_1/src/main.rs
+++ ws_candid/candid_type_1/src/main.rs
@@ -1,3 +1,21 @@
+use candid::CandidType;
+
+#[derive(Debug, Default, CandidType, serde::Serialize)]
+struct Sample {
+    #[serde(rename = "internet-computer")]
+    token: Token
+}
+#[derive(Debug, Default, CandidType, serde::Serialize)]
+struct Token {
+    name: String,
+    symbol: String,
+    decimals: u8,
+    price: u64,
+}
+
 fn main() {
     println!("Hello, world!");
+    let sample = Sample::default();
+    println!("{:?}", &sample);
+    // println!("{:?}", Sample::_ty());
 }
