f2c17e7a539e300138b0eae0a922f3ad69af169d
linnefromice <linnefromice@gmail.com>
2024-05-24 13:35:10 +0900

    update

--- dfx_wrapper_1/src/main.rs
+++ dfx_wrapper_1/src/main.rs
@@ -33,8 +33,6 @@ struct Args {
 }
 
 fn main() {
-    // let runtime = Runtime::new().expect("Unable to create a runtime");
-
     let env_args: Vec<String> = env::args().collect();
     if env_args.len() != 3 {
         println!("Usage: ic-cli <env> <command>");
@@ -44,25 +42,28 @@ fn main() {
         env: NetworkEnv::from(env_args[1].clone()),
         command: env_args[2].clone(),
     };
-    println!("{:?}", args);
 
-    // runtime.block_on(execute());
+    let runtime = Runtime::new().expect("Unable to create a runtime");
+    runtime.block_on(execute(args));
 }
 
-// async fn execute() {
-//     let agent = generate_agent(URL);
-//     // agent.fetch_root_key().await.unwrap();
-//     let subnet_id =
-//         Principal::from_text("2fq7c-slacv-26cgz-vzbx2-2jrcs-5edph-i5s2j-tck77-c3rlz-iobzx-mqe")
-//             .unwrap();
-//     let metrics = agent.read_state_subnet_metrics(subnet_id).await.unwrap();
-//     println!("{:?}", metrics);
-// }
+async fn execute(args: Args) {
+    let agent = generate_agent(args.env.url());
+    if let NetworkEnv::LOCAL = args.env {
+        agent.fetch_root_key().await.unwrap();
+    }
 
-// pub fn generate_agent(url: &str) -> ic_agent::Agent {
-//     ic_agent::Agent::builder()
-//         .with_url(url)
-//         .with_verify_query_signatures(false)
-//         .build()
-//         .unwrap()
-// }
+    let subnet_id =
+        Principal::from_text("2fq7c-slacv-26cgz-vzbx2-2jrcs-5edph-i5s2j-tck77-c3rlz-iobzx-mqe")
+            .unwrap();
+    let metrics = agent.read_state_subnet_metrics(subnet_id).await.unwrap();
+    println!("{:?}", metrics);
+}
+
+pub fn generate_agent(url: &str) -> ic_agent::Agent {
+    ic_agent::Agent::builder()
+        .with_url(url)
+        .with_verify_query_signatures(false)
+        .build()
+        .unwrap()
+}
