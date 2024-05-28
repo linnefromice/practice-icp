9f8b448201f2f71f5b89192a9f068c5449c832c6
linnefromice <linnefromice@gmail.com>
2024-05-28 17:00:34 +0900

    bin wrapper: update

--- ws_dfx/dfx_bin_wrapper/src/dfx_wrapper.rs
+++ ws_dfx/dfx_bin_wrapper/src/dfx_wrapper.rs
@@ -12,16 +12,25 @@ struct ResponseTypePing {
     pub impl_version: Option<String>,
     pub impl_hash: Option<String>,
 }
-pub fn ping(network: Network) {
+pub fn ping(network: Network) -> Result<ResponseTypePing, String> {
     let args = vec!["ping", network.url()];
-    let res = exec_cmd_json_output::<ResponseTypePing>("dfx", &Path::new("."), args);
-    println!("{:?}", res.unwrap());
+    exec_cmd_json_output::<ResponseTypePing>("dfx", &Path::new("."), args)
 }
 
-pub fn canister_create(network: Network) {
+pub fn canister_create(network: Network) -> Result<(), String> {
     let args = vec![vec!["canister", "create", "--all"], network.args()].concat();
-    let res = exec_cmd_output("dfx", &Path::new("."), args);
-    println!("{:?}", res);
+    exec_cmd_none_output("dfx", &Path::new("."), args)
+}
+
+pub fn exec_cmd_none_output(
+    cmd: &str,
+    execution_dir: &Path,
+    args: Vec<&str>,
+) -> Result<(), String> {
+    match exec_cmd_output(cmd, execution_dir, args) {
+        Ok(_stdout) => Ok(()),
+        Err(stderr) => Err(std::str::from_utf8(&stderr).unwrap().to_string()),
+    }
 }
 
 pub fn exec_cmd_string_output(
