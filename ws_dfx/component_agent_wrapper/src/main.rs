509efd7e57fb9fe56dfca513503fb204302f6220
linnefromice <linnefromice@gmail.com>
2024-06-05 15:31:45 +0900

    call canister's some functions & get result to wait by request id

--- ws_dfx/component_agent_wrapper/src/main.rs
+++ ws_dfx/component_agent_wrapper/src/main.rs
@@ -1,16 +1,16 @@
 use std::env;
 
-use candid::{Decode, Encode, Principal};
+use candid::{Encode, Principal};
 use ic_agent::Agent;
 use tokio::runtime::Runtime;
-use types::CycleManagements;
+use types::{CycleManagements, FunctionName};
 
 mod types;
 
 #[derive(Debug)]
 pub struct Args {
     pub target: Principal,
-    pub method_name: String,
+    pub method_name: FunctionName,
 }
 
 fn main() {
@@ -24,7 +24,7 @@ fn main() {
     }
     let args = Args {
         target: Principal::from_text(&env_args[1]).unwrap(),
-        method_name: env_args[2].clone(),
+        method_name: FunctionName::from(env_args[2].as_str()),
     };
 
     Runtime::new()
@@ -44,12 +44,35 @@ async fn execute(args: Args) -> anyhow::Result<()> {
         method_name,
     } = args;
 
-    let call_args = Encode!(&types::Env::LocalDevelopment, &CycleManagements::default())?;
-    agent
-        .update(&target, method_name)
+    let call_args = match method_name {
+        FunctionName::InitIn => {
+            Encode!(&types::Env::LocalDevelopment, &CycleManagements::default())
+        }
+        FunctionName::Setup => {
+            // note: support only relayer
+            let contract_addr = "0539a0EF8e5E60891fFf0958A059E049e43020d9";
+            let web3_ctx_param = types::Web3CtxParam {
+                env: types::Env::LocalDevelopment,
+                url: "https://eth.llamarpc.com".to_string(),
+                from: None,
+                chain_id: 1u64,
+            };
+            let src_component_id = target.to_text(); // ex: "bnz7o-iuaaa-aaaaa-qaaaa-cai";
+            Encode!(&contract_addr, &web3_ctx_param, &src_component_id)
+        }
+        FunctionName::SetTask => Encode!(&60u32, &0u32, &false),
+    }?;
+    println!("method_name: {:?}", &method_name.to_string());
+    println!("call_args: {:?}", call_args);
+
+    let request_id = agent
+        .update(&target, method_name.to_string())
         .with_arg(call_args)
         .call()
         .await?;
+    println!("{:?}", request_id);
+    let res = agent.wait(request_id, target).await?;
+    println!("{:?}", res);
 
     Ok(())
 }
--- ws_dfx/component_agent_wrapper/src/types.rs
+++ ws_dfx/component_agent_wrapper/src/types.rs
@@ -1,5 +1,31 @@
-use candid::CandidType;
+use candid::{de, CandidType};
 use serde::Deserialize;
+
+#[derive(Debug)]
+pub enum FunctionName {
+    InitIn,
+    Setup,
+    SetTask,
+}
+impl From<&str> for FunctionName {
+    fn from(s: &str) -> Self {
+        match s {
+            "init_in" => FunctionName::InitIn,
+            "setup" => FunctionName::Setup,
+            "set_task" => FunctionName::SetTask,
+            _ => panic!("Invalid function name"),
+        }
+    }
+}
+impl FunctionName {
+    pub fn to_string(&self) -> String {
+        match self {
+            FunctionName::InitIn => "init_in".to_string(),
+            FunctionName::Setup => "setup".to_string(),
+            FunctionName::SetTask => "set_task".to_string(),
+        }
+    }
+}
 
 #[derive(CandidType, Deserialize)]
 pub enum Env {
@@ -22,4 +48,12 @@ pub struct CycleManagements {
     pub refueling_interval: u64,
     pub proxy: CycleManagement,
     pub indexer: CycleManagement,
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct Web3CtxParam {
+    pub env: Env,
+    pub url: String,
+    pub from: Option<String>,
+    pub chain_id: u64,
 }
